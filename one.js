cd7063b7d46e10c4c9d68a11d219af690160ead1
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-01-29 11:45:17 +0530

    Js File

--- one.js
+++ one.js
@@ -1,15 +1,28 @@
 var favChannels = [
-    { img_url:"\Amazon\media\LionsgatePlay.jpg"},
-    { img_url:"\Amazon\media\Discovery.jpg"},
-    { img_url:"\Amazon\media\BBC_PLAYER.png"},
-    { img_url:"\Amazon\media\MANORAMAMAX.jpg"},
-    { img_url:"\Amazon\media\hoichoi.jpg"},
-    { img_url:"\Amazon\media\Chaupal.png"},
-    { img_url:"\Amazon\media\Anime.jpg"},
-    { img_url:"\Amazon\media\Erosnow.jpg"},
-    { img_url:"\Amazon\media\Mubi.jpg"},
-    { img_url:"\Amazon\media\Vrott.png"},
-    { img_url:"\Amazon\media\Stingray.png"},
-    { img_url:"\Amazon\media\Docubay.jpg"},
-    
-]
+    { img_url: "/Amazon/media/LionsgatePlay.jpg" },
+    { img_url: "/Amazon/media/Discovery.jpg" },
+    { img_url: "/Amazon/media/BBC_PLAYER.png" },
+    { img_url: "/Amazon/media/MANORAMAMAX.jpg" },
+    { img_url: "/Amazon/media/hoichoi.jpg" },
+    { img_url: "/Amazon/media/Chaupal.png" },
+    { img_url: "/Amazon/media/Anime.jpg" },
+    { img_url: "/Amazon/media/Erosnow.jpg" },
+    { img_url: "/Amazon/media/Mubi.jpg" },
+    { img_url: "/Amazon/media/Vrott.png" },
+    { img_url: "/Amazon/media/Stingray.png" },
+    { img_url: "/Amazon/media/Docubay.jpg" },
+];
+
+favChannels.map(function (elem) {
+    var div = document.createElement('div');
+    div.setAttribute("class", "channelDiv");
+
+    var image = document.createElement('img');
+    image.setAttribute("src", elem.img_url);
+
+    div.append(image);
+    document.querySelector(".channelBtn").append(div);
+
+});
+
+localStorage.setItem("favChannels", JSON.stringify(favChannels));
