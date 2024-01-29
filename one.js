1add7070d7273e5282fb82c3e2951dc18c251b51
ynit2000 <84170843+ynit2000@users.noreply.github.com>
2024-01-29 10:19:51 +0530

    JS file

--- one.js
+++ one.js
@@ -1,29 +1,15 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Toggle menu dropdown on menu button click
-    document.querySelector('.dropdown').addEventListener('click', function (event) {
-        event.stopPropagation(); // Prevents the click event from reaching the document
-        var dropdownContent = document.querySelector('.dropdown-content');
-        dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
-    });
-
-    // Toggle profile options on profile icon click
-    document.getElementById('profileIcon').addEventListener('click', function (event) {
-        event.stopPropagation(); // Prevents the click event from reaching the document
-        var profileOptions = document.getElementById('profileOptions');
-        profileOptions.style.display = (profileOptions.style.display === 'block') ? 'none' : 'block';
-    });
-
-    // Close dropdowns when clicking outside of them
-    document.addEventListener('click', function () {
-        var dropdownContent = document.querySelector('.dropdown-content');
-        var profileOptions = document.getElementById('profileOptions');
-
-        if (dropdownContent.style.display === 'block') {
-            dropdownContent.style.display = 'none';
-        }
-
-        if (profileOptions.style.display === 'block') {
-            profileOptions.style.display = 'none';
-        }
-    });
-});
+var favChannels = [
+    { img_url:"\Amazon\media\LionsgatePlay.jpg"},
+    { img_url:"\Amazon\media\Discovery.jpg"},
+    { img_url:"\Amazon\media\BBC_PLAYER.png"},
+    { img_url:"\Amazon\media\MANORAMAMAX.jpg"},
+    { img_url:"\Amazon\media\hoichoi.jpg"},
+    { img_url:"\Amazon\media\Chaupal.png"},
+    { img_url:"\Amazon\media\Anime.jpg"},
+    { img_url:"\Amazon\media\Erosnow.jpg"},
+    { img_url:"\Amazon\media\Mubi.jpg"},
+    { img_url:"\Amazon\media\Vrott.png"},
+    { img_url:"\Amazon\media\Stingray.png"},
+    { img_url:"\Amazon\media\Docubay.jpg"},
+    
+]
